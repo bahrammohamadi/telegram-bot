8daab22f55ff3799ce3660c8aedf4f0738b4bf8c
bahrammohamadi <lasvaram@gmail.com>
2025-04-12 13:42:15 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-# config.py
+# توکن ربات تلگرام
 BOT_TOKEN = "7730961238:AAGbwrC8NM-DDlHl5ztTmigE-4R9V-RPbY8"
+
+# آی‌دی مدیر ربات
 ADMIN_ID = 46428047
+
+# حالت‌های مختلف بازار (سهام و رمزارزها)
 MARKET_MODE = ["stock", "crypto"]
